dabcb0bf9db9a322bc2c47949c4e1affb02449a9
mennatarekk566 <mennatarek273994@gmail.com>
2026-09-09 00:07:54 +0300

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_ASP_NET_99_C_Basics_05m
+{
+    internal class Book
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_ASP_NET_99_C_Basics_05m
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
